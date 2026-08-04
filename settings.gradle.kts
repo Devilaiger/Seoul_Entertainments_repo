87767a6111405e1de42b96548f51c338437e596b
Devilaiger <devilaiger@gmail.com>
2026-08-04 19:01:27 +0530

    Fix build workflow to dynamically build all active providers and remove root cs3 binaries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,9 @@ rootProject.name = "CloudstreamPlugins"
 // Automatically include any subdirectory that contains a build.gradle.kts file
 rootDir.listFiles()?.forEach { dir ->
     if (dir.isDirectory && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
+        val name = dir.name
+        if (name != "Movix" && name != "LayarKaca" && name != "LayarKacaProvider") {
+            include(name)
+        }
     }
 }
